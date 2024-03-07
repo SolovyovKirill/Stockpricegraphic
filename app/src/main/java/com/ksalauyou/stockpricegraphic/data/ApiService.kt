e93b747fbc98f58b1fb67caaa137d1d4b3406a74
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2024-03-07 13:54:00 +0300

    add scaling

--- app/src/main/java/com/ksalauyou/stockpricegraphic/data/ApiService.kt
+++ app/src/main/java/com/ksalauyou/stockpricegraphic/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=Cp7ge7mELiipsNTfQi0r94EPP3PXMuE0")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=Cp7ge7mELiipsNTfQi0r94EPP3PXMuE0")
     suspend fun loadBars(): Result
 }
--- app/src/main/java/com/ksalauyou/stockpricegraphic/presentation/Terminal.kt
+++ app/src/main/java/com/ksalauyou/stockpricegraphic/presentation/Terminal.kt
@@ -2,31 +2,49 @@ package com.ksalauyou.stockpricegraphic.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.ksalauyou.stockpricegraphic.data.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(bars: List<Bar>) {
+    var visibleBarsCount by remember {
+        mutableStateOf(100)
+    }
+
+    val transformableState = TransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
+    }
     Canvas(modifier = Modifier
         .fillMaxSize()
         .background(Color.Black)
+        .transformable(transformableState)
     ) {
-        bars.forEachIndexed { index, bar ->
+        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
             val max = bars.maxOf { it.high }
             val min = bars.minOf { it.low }
-            val barWidth = size.width / bars.size
-            val offsetX = barWidth * index
+            val barWidth = size.width / visibleBarsCount
+            val offsetX = size.width - barWidth * index
             val pxPerPoint = size.height / (max - min)
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
                 end = Offset(offsetX    , size.height - ((bar.high - min) * pxPerPoint)),
-                strokeWidth = barWidth
+                strokeWidth = 1f
             )
         }
 
