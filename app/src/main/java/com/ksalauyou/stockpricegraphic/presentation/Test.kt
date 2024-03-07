854ea192f08b4f8291e6926dd2de152e5b21acb4
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2024-03-07 18:01:00 +0300

    add custom saver with several parameters

--- app/src/main/java/com/ksalauyou/stockpricegraphic/presentation/Test.kt
+++ app/src/main/java/com/ksalauyou/stockpricegraphic/presentation/Test.kt
@@ -1,6 +1,5 @@
 package com.ksalauyou.stockpricegraphic.presentation
 
-import android.os.Parcelable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +9,16 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.parcelize.Parcelize
 
 @Composable
 fun Test() {
     var testData by rememberSaveable(saver = TestData.Saver) {
-        mutableStateOf(TestData(0))
+        mutableStateOf(TestData(0, "Text"))
     }
     Box(
         modifier = Modifier
@@ -33,15 +32,18 @@ fun Test() {
 }
 
 data class TestData(
-    val number: Int
+    val number: Int,
+    val text: String
 ){
     companion object {
-        val Saver: Saver<MutableState<TestData>, Int> = Saver(
+        val Saver: Saver<MutableState<TestData>, Any> = listSaver(
             save = {
-                it.value.number
+                val testData = it.value
+                listOf(testData.number, testData.text)
             },
             restore = {
-                mutableStateOf(TestData(it))
+                val testData = TestData(number = it[0] as Int, text = it[1] as String)
+                mutableStateOf(testData)
             }
         )
     }
